cec3aef2de18e9a222e9411adcecf5f9413eab84
Arnav Saxena <arnavsaxena17@gmail.com>
2024-12-23 14:52:49 +0530

    changes

--- packages/twenty-server/src/engine/core-modules/arx-chat/services/candidate-engagement/update-chat.ts
+++ packages/twenty-server/src/engine/core-modules/arx-chat/services/candidate-engagement/update-chat.ts
@@ -234,7 +234,8 @@ export class FetchAndUpdateCandidatesChatsWhatsapps {
       });
 
       const data = await axiosRequest(graphqlQueryObj, apiToken);
-      console.log("This is the number of perople who edges messaged recently in getRecentCandidateIds", data?.data?.whatsappMessages?.edges);
+      console.log("This is the number of perople who edges data data messaged recently in getRecentCandidateIds", data?.data);
+      console.log("This is the number of perople who edges data messaged recently in getRecentCandidateIds", data);
       console.log("This is the number of perople who messaged recently in getRecentCandidateIds", data?.data?.whatsappMessages?.edges?.length);
       // Extract unique candidate IDs
       if (data?.data?.whatsappMessages?.edges?.length > 0) {
